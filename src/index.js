63b3f26fed8a9239d2b472edbfcfa21bce9faf89
Jesus <jesusadrian1953.1@gmail.com>
2021-05-25 19:19:15 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client();
+
+client.login("ODQ2ODUzMTEyMTM5NzQzMjMz.YK1jaw.8ErpDGzanP35pUVpuuBZPJjOvrc");
+
+client.on("ready", function () {
+  console.log(`the bot is ready, ${client.user.tag}`);
+  client.user.setStatus("online");
+  const testChannel = client.channels;
+});
+
+client.on("message", async function (msg) {
+  //receiving the message
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+
+  if (msg.content === "hello") {
+    msg.channel.send(`Hi there! ${msg.author}`);
+  }
+
+  if (msg.content.includes("!test")) {
+    msg.channel.send("Glad you are testing");
+  }
+
+  if (msg.content === "ye") {
+    msg.channel.send("ye ye ye!");
+  }
+
+  if (msg.content === "ye ye ye") {
+    msg.channel.send("ye!");
+  }
+
+  if (msg.content === "!pretty") {
+    try {
+      const embed = new MessageEmbed()
+        // .addField("Something one", "Lorem ipsu")
+        .setTitle("Ye")
+        .setColor("RED")
+        .setAuthor(
+          "El kañas",
+          "https://web.whatsapp.com/249c7c0f-556d-4c3b-9dc4-db37719bdc6e"
+        );
+      msg.channel.send(embed);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
